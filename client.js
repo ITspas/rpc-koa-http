a12555d57017a351fc12ef1bf80001383bedff46
2ustc <Netxy@vip.qq.com>
2017-04-25 11:02:36 +0800

    update

--- client.js
+++ client.js
@@ -1,54 +1,44 @@
 var unflatten = require('flat').unflatten
-
-, makeError = function(obj) {
-  var err = new Error(obj.message)
-
-  Object.keys(obj).forEach(function(key) {
-    err[key] = obj[key]
-  })
-
-  return err
-}
-
-, setupClient = function(request) {
-  return function(options) {
-    var remote = {},
-      encoding = options.encoding || JSON
-
-    options.methodNames.forEach(function(name) {
-      remote[name] = function() {
-        var args = Array.prototype.slice.call(arguments),
-          sync = typeof(args[args.length - 1]) !== 'function',
-          callback = sync ? undefined : args.pop()
-
-        request({
-          url: options.url + '/' + name,
-          method: 'POST',
-          body: encoding.stringify({
-            args: args,
-            sync: sync
-          }),
-          timeout: options.timeout || 30 * 1000,
-          withCredentials: options.credentials
-        }, function(err, resp, body) {
-          var args;
-          if (sync) return
-          if (err) return callback(err)
-
-          try {
-            args = encoding.parse(body)
-            if (args[0]) args[0] = makeError(args[0])
-          } catch (err) {
-            return callback(err)
-          }
-
-          callback.apply(null, args)
-        })
-      }
-    })
-
-    return unflatten(remote)
-  }
-}
-
+	, makeError = function (obj) {
+		var err = new Error(obj.message)
+		Object.keys(obj).forEach(function (key) {
+			err[key] = obj[key]
+		})
+		return err
+	}
+	, setupClient = function (request) {
+		return function (options) {
+			var remote = {},
+				encoding = options.encoding || JSON
+			options.methodNames.forEach(function (name) {
+				remote[name] = function () {
+					var args = Array.prototype.slice.call(arguments),
+						sync = typeof (args[args.length - 1]) !== 'function',
+						callback = sync ? undefined : args.pop()
+					request({
+						url: options.url + '/' + name,
+						method: 'POST',
+						body: encoding.stringify({
+							args: args,
+							sync: sync
+						}),
+						timeout: options.timeout || 30 * 1000,
+						withCredentials: options.credentials
+					}, function (err, resp, body) {
+						var args;
+						if (sync) return
+						if (err) return callback(err)
+						try {
+							args = encoding.parse(body)
+							if (args[0]) args[0] = makeError(args[0])
+						} catch (err) {
+							return callback(err)
+						}
+						callback.apply(null, args)
+					})
+				}
+			})
+			return unflatten(remote)
+		}
+	}
 module.exports = setupClient
