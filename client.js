ad6cd8bb2d7f913aed295bfb536f958c4ba917e9
2ustc <Netxy@vip.qq.com>
2017-06-07 19:18:40 +0800

    support alias

--- client.js
+++ client.js
@@ -1,19 +1,45 @@
-var unflatten = require('flat').unflatten
-	, makeError = function (obj) {
+var unflatten = require('flat').unflatten,
+	makeError = function(obj) {
 		var err = new Error(obj.message)
-		Object.keys(obj).forEach(function (key) {
+		Object.keys(obj).forEach(function(key) {
 			err[key] = obj[key]
 		})
 		return err
-	}
-	, setupClient = function (request) {
-		return function (options) {
+	},
+	doAliasDecode = function(alias, aliasMethod, args) {
+		if (aliasMethod) {
+			var oArgs = args,
+				nArgs = {},
+				tmpArr;
+			typeof aliasMethod == 'string' && (aliasMethod = alias[aliasMethod]);
+			if (Array.isArray(aliasMethod)) {
+				aliasMethod.forEach(function(k, i) {
+					if (typeof k == 'string') {
+						nArgs[k] = args[i];
+					} else if (Array.isArray(k) && k.length > 1) {
+						if (k.length > 2) {
+							nArgs[k[0]] = (tmpArr = []);
+							args[i].forEach(function(args) {
+								tmpArr.push(doAliasDecode(alias, k[1], args));
+							});
+						} else {
+							nArgs[k[0]] = doAliasDecode(alias, k[1], args[i]);
+						}
+					}
+				});
+				args = nArgs;
+			}
+		}
+		return args;
+	},
+	setupClient = function(request) {
+		return function(options) {
 			var remote = {},
 				encoding = options.encoding || JSON
-			options.methodNames.forEach(function (name) {
-				remote[name] = function () {
+			options.methodNames.forEach(function(name) {
+				remote[name] = function() {
 					var args = Array.prototype.slice.call(arguments),
-						sync = typeof (args[args.length - 1]) !== 'function',
+						sync = typeof(args[args.length - 1]) !== 'function',
 						callback = sync ? undefined : args.pop()
 					request({
 						url: options.url + '/' + name,
@@ -24,13 +50,16 @@ var unflatten = require('flat').unflatten
 						}),
 						timeout: options.timeout || 30 * 1000,
 						withCredentials: options.credentials
-					}, function (err, resp, body) {
+					}, function(err, resp, body) {
 						var args;
 						if (sync) return
 						if (err) return callback(err)
 						try {
 							args = encoding.parse(body)
-							if (args[0]) args[0] = makeError(args[0])
+							if (args[0]) args[0] = makeError(args[0]);
+							if (options.methodsAlias[name] && args.length > 2) {
+								args[2] = doAliasDecode(options.alias, options.methodsAlias[name], args[2])
+							}
 						} catch (err) {
 							return callback(err)
 						}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var collect = require('collect-stream'),
 	serializeError = require('serialize-error'),
 	slice = Array.prototype.slice;
+
 function endWithJson(statusCode, json, cxt, encoding, done) {
 	var payload = new Buffer(encoding.stringify(json));
 	cxt.set('content-type', 'application/json');
@@ -9,11 +10,13 @@ function endWithJson(statusCode, json, cxt, encoding, done) {
 	cxt.body = payload;
 	done && done();
 }
+
 function error(encoding, err, res, done) {
 	endWithJson(500, [serializeError(err)], res, encoding, done)
 }
+
 function parseResponse(encoding, stream, callback) {
-	collect(stream, function (err, buffer) {
+	collect(stream, function(err, buffer) {
 		if (err) return callback(err)
 		try {
 			callback(null, encoding.parse(buffer.toString()))
@@ -22,9 +25,38 @@ function parseResponse(encoding, stream, callback) {
 		}
 	})
 }
-function doParseRequest(encoding, cxt, fun) {
-	return function (done) {
-		parseResponse(encoding, cxt.req, function (err, input) {
+
+function doAliasEncode(alias, aliasMethod, args) {
+	if (aliasMethod) {
+		var oArgs = args,
+			nArgs = [],
+			tmpArr;
+		typeof aliasMethod == 'string' && (aliasMethod = alias[aliasMethod]);
+		if (Array.isArray(aliasMethod)) {
+			aliasMethod.forEach(function(k) {
+				if (typeof k == 'string') {
+					nArgs.push(args[k])
+				} else if (Array.isArray(k) && k.length > 1) {
+					if (k.length > 2) {
+						nArgs.push((tmpArr = []));
+						args[k[0]].forEach(function(args) {
+							tmpArr.push(doAliasEncode(alias, k[1], args));
+						});
+					} else {
+						nArgs.push(doAliasEncode(alias, k[1], args[k[0]]))
+					}
+				}
+			});
+			args = nArgs;
+		}
+	}
+	return args;
+}
+
+
+function doParseRequest(encoding, cxt, fun, alias, aliasMethod) {
+	return function(done) {
+		parseResponse(encoding, cxt.req, function(err, input) {
 			if (err) {
 				error(encoding, err, cxt, done);
 				return
@@ -34,11 +66,12 @@ function doParseRequest(encoding, cxt, fun) {
 					fun.apply(cxt, input.args)
 					done();
 				} else {
-					input.args.push(function () {
+					input.args.push(function() {
 						var args = slice.call(arguments)
 						if (args[0]) {
 							args[0] = serializeError(args[0])
 						}
+						if (aliasMethod && args.length > 2) args[2] = doAliasEncode(alias, aliasMethod, args[2])
 						endWithJson(200, args, cxt, encoding, done);
 					})
 					fun.apply(cxt, input.args)
@@ -50,9 +83,9 @@ function doParseRequest(encoding, cxt, fun) {
 	}
 }
 
-module.exports = function (options) {
+module.exports = function(options) {
 	var encoding = options.encoding || JSON;
-	return function* () {
+	return function*() {
 		options.origin && this.req.headers && this.req.headers.origin && (this.set('Access-Control-Allow-Origin', this.req.headers.origin));
 		options.credentials && (this.set('Access-Control-Allow-Credentials', true));
 		if (this.url.slice(0, options.url.length) !== options.url) return
@@ -62,6 +95,6 @@ module.exports = function (options) {
 			error(encoding, new Error('No method ' + methodName), this)
 			return
 		}
-		yield doParseRequest(encoding, this, fun);
+		yield doParseRequest(encoding, this, fun, options.alias, options.methodsAlias[methodName]);
 	}
 }
